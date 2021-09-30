22b661a779ee72dc2527cad5b13bef0834d7ba4d
HKMC\E183999 <limminjeong98@naver.com>
2021-09-30 10:34:28 +0900

    HTTP 요청 - 기본, 헤더 조회

--- src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -24,14 +24,29 @@ public class RequestHeaderController {
                           @RequestHeader("host") String host,
                           @CookieValue(value = "myCookie", required = false) String cookie) {
 
+        /**
+         * MultiValueMap: 하나의 key에 여러 value를 배열로 저장할 수 있다
+         * HTTP header, HTTP 쿼리 파라미터와 같이 하나의 키에 여러 값을 받을 때 사용한다.
+         * keyA=value1&keyA=value2
+         *
+         * MultiValueMap<String, String> map = new LinkedMultiValueMap();
+         * map.add("keyA", "value1");
+         * map.add("keyA", "value2");
+         *
+         * List<String> values = map.get("keyA"); // [value1,value2]
+         * 
+         * 여기서 Headers에 Accept를 중복해서 요청하면 배열로 반환된다
+         * headerMap={accept=[application/json, text/html], user-agent=[PostmanRuntime/7.28.4], postman-token=[1aa1b05b-1fa3-4945-9d7d-1db15ece6bac], host=[localhost:8080], accept-encoding=[gzip, deflate, br], connection=[keep-alive]}
+         */
         log.info("request={}", request);
         log.info("response={}", response);
         log.info("httpMethod={}", httpMethod);
-        log.info("locale={}", locale);
-        log.info("headerMap={}", headerMap);
-        log.info("header host={}", host);
+        log.info("locale={}", locale); // 언어 정보
+        log.info("headerMap={}", headerMap); // 헤더 정보
+        log.info("header host={}", host); // 헤더의 단일 정보 받고 싶을 때
         log.info("myCookie={}", cookie);
 
         return "ok";
     }
+
 }
