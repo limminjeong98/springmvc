e96d56bb0edb14ac2c6f09ec24ce6c9956472fe8
HKMC\E183999 <limminjeong98@naver.com>
2021-10-01 16:36:01 +0900

    HTTP 응답 - HTTP API, 메시지 바디에 직접 입력

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,64 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+// @RestController = @Controller + @ResponseBody / 클래스 단위에서 붙일 수 있다
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * HttpEntity, ResponseEntity(Http Status 추가)
+     *
+     * @return
+     */
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    // ResponseEntity는 HttpStatus 상태코드를 지정할 수 있지만,
+    // ResponseBody를 사용해 직접 HelloData 객체를 반환하면 상태코드를 지정할 수 없다
+    // 그래서 @ResponseStatus annotation을 이용해서 상태코드를 지정한다
+    // 만약 동적으로 상태코드를 변경하고 싶으면 ResponseEntity(HttpEntity)를 사용해야한다
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return helloData;
+    }
+}
